56a613aea088ef90caabeb84d78fefec5d6407d4
Jorge Morales <123045839@upq.edu.mx>
2026-06-23 22:54:54 -0600

    practica 12
    practica 12

--- Practica5/miapp2/screens/MenuScreen.js
+++ Practica5/miapp2/screens/MenuScreen.js
@@ -1,48 +1,51 @@
 //Zona 1 de Importaciones, componentes y archivos 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, View, Button } from 'react-native';
 import React, { useState } from 'react';
 import TargetasScreen from './TargetasScreen';
 import SafeScreen from './SafeScreen';
 import PresseableScreen from './PresseableScreen';
 import ModalBottomSheetScreen from './ModalBottomSheetScreen';
+import TextInputAlerts from './TextInputAlerts';
 
 //zona 2 : Main y componentes
 export default function MenuScreen() {
-const [screen, setScreen] = useState('menu');
+  const [screen, setScreen] = useState('menu');
 
-    switch(screen){
-        case 'targetas':
-            return <TargetasScreen/>;
-        case 'safeArea':
-            return <SafeScreen/>;  
-        case 'presseable':
-            return <PresseableScreen/>;
-        case 'modal':
-            return <ModalBottomSheetScreen/>;
-        case 'menu':
-        default:       
-  
-    return (
-    <View style={styles.container}>
-        
-      <View> <Button title='Practica Targetas' onPress={() => setScreen('targetas')} />
-      </View>
-
-      <View> <Button title='Practica SafeArea' onPress={() => setScreen('safeArea')} />
-      </View>
-
-      <View> <Button title='Practica Presseable' onPress={() => setScreen('presseable')} />
-      </View>
-
-      <View> <Button title='Practica Modal' onPress={() => setScreen('modal')} />
-      </View>
-
-      <StatusBar style="auto" />
-      
-    </View>
-  );
-}
+  switch(screen){
+    case 'targetas':
+      return <TargetasScreen/>;
+    case 'safeArea':
+      return <SafeScreen/>;  
+    case 'presseable':
+      return <PresseableScreen/>;
+    case 'modal':
+      return <ModalBottomSheetScreen/>;
+    case 'textinput':
+      return <TextInputAlerts/>;
+    case 'menu':
+    default:       
+      return (
+        <View style={styles.container}>
+          <View> 
+            <Button title='Practica Targetas' onPress={() => setScreen('targetas')} />
+          </View>
+          <View> 
+            <Button title='Practica SafeArea' onPress={() => setScreen('safeArea')} />
+          </View>
+          <View> 
+            <Button title='Practica Presseable' onPress={() => setScreen('presseable')} />
+          </View>
+          <View> 
+            <Button title='Practica Modal' onPress={() => setScreen('modal')} />
+          </View>
+          <View> 
+            <Button title='Practica TextInput' onPress={() => setScreen('textinput')} />
+          </View>
+          <StatusBar style="auto" />
+        </View>
+      );
+  }
 }
 
 //zona 3 : Estilos y pososcionamiento 
@@ -52,6 +55,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
 });
